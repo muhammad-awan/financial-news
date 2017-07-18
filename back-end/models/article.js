d5a23745579ffee35d91f83be30c1738a960bf64
Muhammad Awan <muhammad.adam.awan@gmail.com>
2017-07-18 15:29:55 +1000

    Back-end: Modify article schema

--- back-end/models/article.js
+++ back-end/models/article.js
@@ -11,7 +11,25 @@ const articleSchema = mongoose.Schema({
   create_date: {
     type: Date,
     default: Date.now
-  }
+  },
+  postedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+    },
+  comments: [{
+    text: String,
+    postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+  }],
+  likes: [{
+    type: Number,
+    likedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  }]
 })
 
 const Article = mongoose.model('Article', articleSchema)
