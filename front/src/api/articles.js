a64e6ad299e143203105133a90b59d926529c182
Muhammad Awan <muhammad.adam.awan@gmail.com>
2017-07-21 04:07:49 +1000

    Post articles

--- front/src/api/articles.js
+++ front/src/api/articles.js
@@ -4,3 +4,9 @@ export function list() {
     return api.get('/articles')
         .then(res => res.data)
 }
+
+export function create({ title, url }) {
+    return api.post('/articles', {
+        title, url
+    }).then(res => res.data)
+}
